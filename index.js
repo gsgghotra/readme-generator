d4946cb53ca0d449ec7b3d9956cb38815c5a738d
Gurjeet Singh <gsgghotra@gmail.com>
2023-12-18 20:02:44 +0000

    Draft questions added, inquirer added

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    },
+    {
+    type: 'checkbox',
+    message: 'What languages do you know?',
+    name: 'stack',
+    choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+    type: 'list',
+    message: 'What is your preferred method of communication?',
+    name: 'contact',
+    choices: ['email', 'phone', 'telekinesis'],
+    },
 ];
 
 // function to write README file
@@ -15,7 +31,8 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt(questions)
 }
 
 // function call to initialize program
