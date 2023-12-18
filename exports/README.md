d4e29b60446e3900619550e47a48beadc07a621b
Gurjeet Singh <gsgghotra@gmail.com>
2023-12-18 20:56:03 +0000

    Static structure of README file added

--- exports/README.md
+++ exports/README.md
@@ -1,2 +1,25 @@
-# sfd
+# Testing structure
+
+  ## Description
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+  ## Installation
+
+  ## Usage
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,29 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+  ## Installation
+
+  ## Usage
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
+
 `;
 }
 module.exports = generateMarkdown;
